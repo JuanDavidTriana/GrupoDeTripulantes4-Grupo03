86c92bb148c05d13a33c2219dea878377753a42a
Fabian B <lince.86@gmail.com>
2021-10-09 16:49:24 -0500

    Plantilla final

--- Alejandria/server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'eb0d4499aff1432540a0796c62bd9507'),
-    },
-  },
-});
